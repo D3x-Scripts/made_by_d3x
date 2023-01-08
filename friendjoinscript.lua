30ef8ec600a7de5ce344637e81603d59e8c82c07
Deez-Nuts445 <111050099+Deez-Nuts445@users.noreply.github.com>
2023-01-08 19:57:30 +0800

    Create friendjoinscript.lua

---
+++ friendjoinscript.lua
@@ -0,0 +1,15 @@
+local sys = loadstring(game:HttpGet("https://raw.githubusercontent.com/Deez-Nuts445/GHSandbox_LUA_1/main/notification.lua"))()
+local plrs = game.Players
+local lp = plrs.LocalPlayer
+
+plrs.PlayerAdded:Connect(function(plr)
+if plr:IsFriendsWith(lp.UserId) then
+sys:notif("Your friend "..plr.Name.." has joined the game!",3)
+end
+end)
+
+plrs.PlayerRemoving:Connect(function(plr)
+if plr:IsFriendsWith(lp.UserId) then
+sys:notif("Your friend "..plr.Name.." has left the game!",3)
+end
+end)
